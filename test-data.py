723c355933c537ad82fa245c41f2925593886938
Debjyoti Chowdhury <djbabai.debjyoti@gmail.com>
2025-03-24 16:46:20 +0530

    synthetic data

--- test-data.py
+++ test-data.py
@@ -1,7 +1,7 @@
 from os import listdir
 from os.path import join
 import matplotlib.pyplot as plt
-from PIL import Image  # Import PIL to open images
+from PIL import Image, ImageFilter  # Import PIL to open images
 
 im_path = "images/generated/gauges"
 
@@ -9,10 +9,17 @@ gauge_images = listdir(im_path)
 
 for im in gauge_images:
     full_path = join(im_path, im)
-    image = Image.open(full_path)  # Open the image file
+    image = Image.open(full_path)
+    image = image.convert("L")
+    image = image.filter(ImageFilter.FIND_EDGES)
+    class_name = im.split("-")
+
+    print(class_name)
+
     plt.clf()
+    plt.title(f"{class_name}")
     plt.imshow(image)
-    plt.axis('off')  # Hide axis for better viewing
+    # plt.axis('off')  # Hide axis for better viewing
     plt.pause(1)
 
 
