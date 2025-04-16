440e07663fbe7f72aeaa4fcae83d00d3b6726d70
Debjyoti Chowdhury <djbabai.debjyoti@gmail.com>
2025-04-16 15:52:04 +0530

    updated image gen

---
+++ test-images.py
@@ -0,0 +1,13 @@
+import os
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+NEEDLE = 'images/reference/gauge-needle.png'
+GAUGE = 'images/reference/gauge-dial.png'
+NEEDLE_ZERO = 'images/reference/gauge-needle_rot_0.png'
+
+
+
+
